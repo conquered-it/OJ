adc46364a0ef468ad7702209835913d8117295ee
conquered-it <gps.vsgg@gmail.com>
2021-01-27 18:00:18 +0530

    v1.0.3

--- index.js
+++ index.js
@@ -2,22 +2,43 @@ var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
 request = require('request'),
-// mongoose = require("mongoose"),
+mongoose = require("mongoose"),
 port = process.env.PORT || 3000;
-// mongoose.set('useNewUrlParser', true);
-// mongoose.connect("mongodb://localhost/oj",{ useUnifiedTopology: true });
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb://localhost/oj",{ useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static('views'));
 
+var problemSchema = new mongoose.Schema({
+    title:String,
+    statement:String,
+    tests:[String]
+})
+
+var problems = mongoose.model('problems',problemSchema);
+
 app.get('/',function(req,res){
     res.render('show');
 })
-app.get('https://rextester.com/rundotnet/api',function(req,res){
-    res.send('here');
+
+app.get('/problems',function(req,res){
+    res.render('problems');
 })
+
+app.get('/problems/new',function(req,res){
+    res.render('new_problems');
+})
+
+app.post('/problems',function(req,res){
+    problems.create(req.body.prob,function(err,ret){
+        if(err) console.log('err');
+        else console.log(ret);
+    })
+})
+
 app.post('/result',function(req,res){
-    console.log(req.body);
+    // console.log(req.body);
     res.send({});
 })
 /*
